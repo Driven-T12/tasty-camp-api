41169efd3acf6fdad93b5c0e61c27e3cf9998991
leticia.chijo <leticia.chijo@gmail.com>
2023-08-17 19:22:35 -0300

    Seção: async/await e try/catch

--- src/app.js
+++ src/app.js
@@ -3,56 +3,68 @@ import cors from 'cors';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from "dotenv";
 
-const app = express(); // criando a aplicaçãp servidora
-app.use(cors()); // estou tornando publico o acesso a minha API
-app.use(express.json()); // dizendo para o servidor que o padrão de dados das requisições é json
-dotenv.config(); // permitir acesso ao arquivo .env (variáveis de ambiente)
+const app = express();      // criando a aplicaçãp servidora
+app.use(cors());            // estou tornando publico o acesso a minha API
+app.use(express.json());    // dizendo para o servidor que o padrão de dados das requisições é json
+dotenv.config();            // permitir acesso ao arquivo .env (variáveis de ambiente)
 
 // Conexão com o mongoDB
-const mongoClient = new MongoClient(process.env.DATABASE_URL) // Conexão localhost mongo
-let db
+const mongoClient = new MongoClient(process.env.DATABASE_URL)   // Conexão localhost mongo
 
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch(err => console.log(err.message))
+try {
+    // Top level await => await que está no escopo global da sua aplicação, não precisa de async
+    await mongoClient.connect()
+    console.log("MongoDB conectado!")
+} catch (err) {
+    console.log(err.message)
+}
+
+const db = mongoClient.db()
+
 
 // Rotas
-app.get('/receitas', (request, response) => {
-    db.collection("receitas").find().toArray()
-        .then(receitas => response.send(receitas))
-        .catch(erro => response.status(500).send(erro.message));
+app.get('/receitas', async (request, response) => {
+    try {
+        const receitas = await db.collection("receitas").find().toArray()
+        response.send(receitas)
+    } catch (err) {
+        response.status(500).send(err.message)
+    }
 });
 
-app.get('/receitas/:id', (request, response) => {
+app.get('/receitas/:id', async (request, response) => {
     const { id } = request.params;
 
-    db.collection("receitas").findOne({ _id: new ObjectId(id) })
-        .then(receita => response.send(receita))
-        .catch((err) => response.status(500).send(err))
+    try {
+        const receita = await db.collection("receitas").findOne({ _id: new ObjectId(id) })
+        response.send(receita)
+    } catch (err) {
+        response.status(500).send(err.message)
+    }
 });
 
-app.post('/receitas', (request, response) => {
+app.post('/receitas', async (request, response) => {
     const { titulo, ingredientes, preparo } = request.body;
 
     if (!titulo || !ingredientes || !preparo) {
-        response.status(422).send("Todos os campos devem ser preenchidos!");
-        return;
+        return response.status(422).send("Todos os campos devem ser preenchidos!");
     }
 
     if (ingredientes.length <= 10) {
-        response.status(422).send("Informações de ingredientes deve contar mais de 10 letras");
-        return;
+        return response.status(422).send("Informações de ingredientes deve contar mais de 10 letras");
     }
 
-    const nova = {
-        titulo: titulo,
-        ingredientes: ingredientes,
-        preparo: preparo
-    }
+    const novaReceita = { titulo, ingredientes, preparo }
 
-    db.collection("receitas").insertOne(nova)
-        .then(() => response.status(201).send(nova))
-        .catch((err) => response.status(500).send(err))
+    try {
+        const receitaExiste = await db.collection("receitas").findOne({ titulo })
+        if (receitaExiste) return response.status(409).send("Essa receita já existe!")
+
+        await db.collection("receitas").insertOne(novaReceita)
+        response.status(201).send(novaReceita)
+    } catch (err) {
+        response.status(500).send(err.message)
+    }
 });
 
 app.listen(4000, () => console.log('App Servidor executando na porta 4000'));
